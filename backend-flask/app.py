01327ec7ed2a25f06e32e65c7a432830a639a219
Abdelrhman Kamal <abdelrhmankamal23@gmail.com>
2023-03-04 13:49:44 +0200

    add xray

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -14,13 +14,25 @@ from services.messages import *
 from services.create_message import *
 from services.show_activity import *
 
+# Cloudwatch logs
+import watchtower
+import logging
+from time import strftime
+
 # HONEYCOMP
 from opentelemetry import trace
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import *
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor 
+
+# aws xray sdk for python to trace requests
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+
+app = Flask(__name__)
 
 # HONEYCOMP Initialize tracing and an exporter that can send data to Honeycomb
 provider = TracerProvider()
@@ -31,11 +43,22 @@ provider.add_span_processor(processor)
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
-app = Flask(__name__)
+xray_url = os.getenv("AWS_XRAY_URL")
+xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+XRayMiddleware(app, xray_recorder)
 
 # HONEYCOMP Initialize automatic instrumentation with Flask
 FlaskInstrumentor().instrument_app(app)
 RequestsInstrumentor().instrument()
+
+# Configuring Logger to Use CloudWatch
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+LOGGER.addHandler(console_handler)
+LOGGER.addHandler(cw_handler)
+LOGGER.info("test log")
 
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
@@ -48,6 +71,14 @@ cors = CORS(
   allow_headers="content-type,if-modified-since",
   methods="OPTIONS,GET,HEAD,POST"
 )
+
+# define routes for Cloudwatch logs
+
+@app.after_request
+def after_request(response):
+    timestamp = strftime('[%Y-%b-%d %H:%M]')
+    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+    return response
 
 # define routes for flask app where these routes are the endpoints for the API calls from the frontend app
 # cross_origin() is used to POST data to the API from the frontend app
@@ -92,6 +123,12 @@ def data_home():
   data = HomeActivities.run()
   return data, 200
 
+# logging the request of the home page to Cloudwatch
+""" @app.route("/api/activities/home", methods=['GET'])
+def data_home():
+  data = HomeActivities.run(Logger = LOGGER)
+  return data, 200
+"""
 @app.route("/api/activities/notifications", methods=['GET'])
 def data_notifications():
   data = NotificationsActivities.run()
--- journal/week2.md
+++ journal/week2.md
@@ -1,1 +1,34 @@
 # Week 2 — Distributed Tracing
+
+## span
+
+A span is a unit of work in a distributed system that tells what happened when. It is a logical unit of work that is performed by a service. A span is defined by a start time, a duration, and a set of tags. A span may also have a parent span, which is a span that is a direct ancestor of the current span. A span may also have a set of child spans, which are spans that are direct descendants of the current span. A span may also have a set of logs, which are key-value pairs that are associated with the span.
+
+### Instrument AWS X-Ray for Flask
+
+```sh
+export AWS_REGION="ca-central-1"
+gp env AWS_REGION="ca-central-1"
+```
+
+Set the env var in your backend-flask for [docker-compose.yml](../docker-compose.yml)
+
+```yml
+      AWS_DEFAULT_REGION: "${AWS_DEFAULT_REGION}"
+      AWS_ACCESS_KEY_ID: "${AWS_ACCESS_KEY_ID}"
+      AWS_SECRET_ACCESS_KEY: "${AWS_SECRET_ACCESS_KEY}"
+```
+
+passing AWS_REGION doesn't seems to get picked up by boto3 so will pass default region instead
+
+## SDK
+
+SDK stands for Software Development Kit. It is a set of tools that help developers build applications for a specific platform or programming language. SDKs typically include a library of functions and resources that developers can use to create applications. It also includes documentation and sample code that developers can use to learn how to use the library.
+
+## opentelemetry
+
+OpenTelemetry is a collection of tools, APIs, and SDKs. You can use it to instrument, generate, collect, and export telemetry data (metrics, logs, and traces) for analysis in order to understand your software's performance and behavior.
+
+```bash
+aws xray create-group --group-name "Cruddur" --filter-expression "services(\"backend-flask\")"
+```
